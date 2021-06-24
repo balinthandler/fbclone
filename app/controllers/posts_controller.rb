6addce0abdac0831c23b6db1687d83b55d79e448
Balint Handler <handlerbalint@gmail.com>
2021-06-24 16:28:13 +0200

    blank Post model, user authentication

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+  
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(firstname: "Balint", lastname: "Handler", email: "bh@e.com", password: "hhhhhh")
+User.create(firstname: "John", lastname: "Doe", email: "jd@e.com", password: "hhhhhh")
+User.create(firstname: "Bob", lastname: "Marley", email: "bm@e.com", password: "hhhhhh")
