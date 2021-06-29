6998bec7e250210e6b943fa78b0b90caf1445d19
Balint Handler <handlerbalint@gmail.com>
2021-06-29 16:03:39 +0200

    activestorage for avatar picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
        def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password)}
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :avatar, :lastname, :email, :password)}
 
-            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstname, :lastname, :email, :password, :current_password)}
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstname, :lastname, :email, :avatar, :password, :current_password)}
        end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :pending_invitations, -> { where confirmed: false }, class_name: :Invitation, foreign_key: :friend_id, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
+  after_commit :add_default_avatar, on: [:create, :update]
 
   def friends
     friends_i_sent_invitation = Invitation.where(user_id: id, confirmed: true).pluck(:friend_id)
@@ -19,6 +21,37 @@ class User < ApplicationRecord
 
   def is_friend?(user)
     Invitation.confirmed_record?(id, user.id)
+  end
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize: "300x300!")
+    else
+      "/default_profile.jpg"
+    end
+  end
+
+  def avatar_icon
+    if avatar.attached?
+      avatar.variant(resize: "100x100!")
+    else
+      "/default_profile.jpg"
+    end
+  end
+
+  private
+  def add_default_avatar
+    unless avatar.attached?
+        avatar.attach(
+          io: File.open(
+            Rails.root.join(
+              'app', 'assets', 'images', 'default_profile.jpg'
+            )
+          ),
+          filename: 'default_profile.jpg',
+          content_type: 'image/jpg'
+        )
+    end
   end
 
 end
